e04ffdc7e95a789f4c37d33c5b127d2709813e2b
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-25 21:28:31 +0100

    Ajout des fonction liés au contrat
    J'ai mis dans le dossier src l'implémentation des fonctions du contrat

---
+++ src/write_functions.py
@@ -0,0 +1,32 @@
+import web3
+
+def ajouter_tache(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
+    transaction = contract.functions.ajouter_une_tache(tache).build_transaction(
+        {"gasPrice": w3.eth.gas_price, 
+        "chainId": w3.eth.chain_id, 
+        "from": wallet, 
+        "nonce": w3.eth.get_transaction_count(wallet)})
+    signed_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+
+def tache_accomplie(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
+    transaction = contract.functions.tache_accomplie(tache).build_transaction(
+        {"gasPrice": w3.eth.gas_price, 
+        "chainId": w3.eth.chain_id, 
+        "from": wallet, 
+        "nonce": w3.eth.get_transaction_count(wallet)})
+    signed_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+
+def reset(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str):
+    transaction = contract.functions.reset().build_transaction(
+        {"gasPrice": w3.eth.gas_price, 
+        "chainId": w3.eth.chain_id, 
+        "from": wallet, 
+        "nonce": w3.eth.get_transaction_count(wallet)})
+    signed_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+
