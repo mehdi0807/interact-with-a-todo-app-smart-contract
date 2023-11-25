9d845fa6f6454a163c0b08c112ee3e5bb2a47977
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-25 21:57:21 +0100

    Ajouter un return aux fonctions de lecture

--- src/read_functions.py
+++ src/read_functions.py
@@ -1,13 +1,13 @@
 import web3
 
 def voir_progres(contract: web3._utils.contracts) -> list[list]:
-    contract.functions.voir_progres().call()
+    return contract.functions.voir_progres().call()
 
 def nombre_taches_accomplies(contract: web3._utils.contracts) -> list[list]:
-    contract.functions.nombre_taches_accomplies().call()
+    return contract.functions.nombre_taches_accomplies().call()
 
 def nombre_taches_totales(contract: web3._utils.contracts) -> list[list]:
-    contract.functions.nombre_taches_totoles().call()
+    return contract.functions.nombre_taches_totoles().call()
 
 def pourcentage_accompli(contract: web3._utils.contracts) -> list[list]:
-    contract.functions.pourcentage_accompli().call()
+    return contract.functions.pourcentage_accompli().call()
