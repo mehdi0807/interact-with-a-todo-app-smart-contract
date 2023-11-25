992d75f1c38df0a259d0aaa7f0c96e65071724cb
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-25 21:47:30 +0100

    Modifier le type de la variable contract + .gitignore file

--- src/read_functions.py
+++ src/read_functions.py
@@ -1,13 +1,13 @@
 import web3
 
-def voir_progres(contract: web3._utils.datatypes.Contract) -> list[list]:
+def voir_progres(contract: web3._utils.contracts) -> list[list]:
     contract.functions.voir_progres().call()
 
-def nombre_taches_accomplies(contract: web3._utils.datatypes.Contract) -> list[list]:
+def nombre_taches_accomplies(contract: web3._utils.contracts) -> list[list]:
     contract.functions.nombre_taches_accomplies().call()
 
-def nombre_taches_totales(contract: web3._utils.datatypes.Contract) -> list[list]:
+def nombre_taches_totales(contract: web3._utils.contracts) -> list[list]:
     contract.functions.nombre_taches_totoles().call()
 
-def pourcentage_accompli(contract: web3._utils.datatypes.Contract) -> list[list]:
+def pourcentage_accompli(contract: web3._utils.contracts) -> list[list]:
     contract.functions.pourcentage_accompli().call()
--- src/write_functions.py
+++ src/write_functions.py
@@ -1,6 +1,6 @@
 import web3
 
-def ajouter_tache(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
+def ajouter_tache(contract: web3._utils.contracts, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
     transaction = contract.functions.ajouter_une_tache(tache).build_transaction(
         {"gasPrice": w3.eth.gas_price, 
         "chainId": w3.eth.chain_id, 
@@ -10,7 +10,7 @@ def ajouter_tache(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, w
     transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
     transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
 
-def tache_accomplie(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
+def tache_accomplie(contract: web3._utils.contracts, w3:web3.main.Web3, wallet:str, private_key:str, tache:str):
     transaction = contract.functions.tache_accomplie(tache).build_transaction(
         {"gasPrice": w3.eth.gas_price, 
         "chainId": w3.eth.chain_id, 
@@ -20,7 +20,7 @@ def tache_accomplie(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3,
     transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
     transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
 
-def reset(contract: web3._utils.datatypes.Contract, w3:web3.main.Web3, wallet:str, private_key:str):
+def reset(contract: web3._utils.contracts, w3:web3.main.Web3, wallet:str, private_key:str):
     transaction = contract.functions.reset().build_transaction(
         {"gasPrice": w3.eth.gas_price, 
         "chainId": w3.eth.chain_id, 
